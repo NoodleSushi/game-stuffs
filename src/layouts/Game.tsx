2a34abcfe8b720b6ec79a8f98904ac3721a30010
NoodleSushi <ytmikesstuffs@gmail.com>
2023-09-10 01:00:02 +0800

    style game layout

--- src/layouts/Game.tsx
+++ src/layouts/Game.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { createContext, useState } from "react";
+import styles from './Game.module.css';
 
 // @ts-ignore:next-line
 export const TitleContext = createContext({ title: '', setTitle: (title: string) => { } });
@@ -10,7 +11,9 @@ function Game() {
   return <>
     <h1>{ title }</h1>
     <TitleContext.Provider value={{ title, setTitle }}>
-      <Outlet/>
+      <div className={`${styles.container}`}>
+        <Outlet/>
+      </div>
     </TitleContext.Provider>
   </>
 }
