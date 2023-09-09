aa269d30b760cc814ce052459e27589ae457c636
NoodleSushi <ytmikesstuffs@gmail.com>
2023-09-10 00:59:55 +0800

    style main page

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -2,13 +2,15 @@ import GameOption from '../components/GameOption';
 import styles from './Main.module.css';
 
 function Main() {
-  return <>
+  return <div className={`${styles.root}`}>
     <div className={`${styles.header}`}>
       <h1>Game Stuffs!</h1>
     </div>
-    <GameOption to='/game/tictactoe' label='TicTacToe' />
-    <GameOption to='/game/connect4' label='Connect 4' />
-  </>
+    <div className={`${styles.options}`}>
+      <GameOption to='/game/tictactoe' label='TicTacToe' />
+      <GameOption to='/game/connect4' label='Connect 4' />
+    </div>
+  </div>
 }
 
 export default Main;
