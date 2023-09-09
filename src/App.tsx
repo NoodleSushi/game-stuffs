1ad76660a4b924ef90a7e1ab54c97ee84c0dfc79
NoodleSushi <ytmikesstuffs@gmail.com>
2023-09-09 20:41:08 +0800

    style website

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import Main from './layouts/Main';
 import Game from './layouts/Game';
 import TicTacToe from './games/TicTacToe';
 import Connect4 from './games/Connect4';
+import bg from './assets/papertex.png';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/game' element={<Game />}>
-          <Route path='tictactoe' element={<TicTacToe />} />
-          <Route path='connect4' element={<Connect4 />}/>
-        </Route>
-      </Routes>
-    </HashRouter>
+    <div style={{ backgroundImage: `url(${bg})`, width: `100vw`, height: `100vh` }}>
+      <HashRouter>
+        <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='/game' element={<Game />}>
+              <Route path='tictactoe' element={<TicTacToe />} />
+              <Route path='connect4' element={<Connect4 />}/>
+            </Route>
+        </Routes>
+      </HashRouter>
+    </div>
   )
 }
 
